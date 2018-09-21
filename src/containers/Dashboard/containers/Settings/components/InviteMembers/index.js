fc1c9729ba351172d784ea460f424ad9e8dbef36
Dmytro Nasyrov <dmytro@pharosproduction.com>
2018-09-21 16:46:23 +0300

    try dist

---
+++ src/containers/Dashboard/containers/Settings/components/InviteMembers/index.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import { Container, Row, Col, Jumbotron, Label, Input, Button } from 'reactstrap'
+
+// Component
+
+class InviteMembers extends Component {
+
+  // Render
+
+  render() {
+    return (
+      <div className="settings--invite">
+        <Container fluid>
+          <Jumbotron>
+            <h2>Members Information</h2>
+
+            <Container fluid>
+              {this.props.members.map((el, idx) => {
+                return (
+                  <Row key={idx}>
+                    <Col sm={6}>
+                      <Label for="email">{el[0]['role']}</Label>
+                      <Input type="email" name="email" id="email" placeholder="Email" value={el[0]['email']} bsSize="md" />
+                    </Col>
+
+                    {el[1] && <Col sm={6}>
+                      <Label for="email">{el[1]['role']}</Label>
+                      <Input type="email" name="email" id="email" placeholder="Email" value={el[1]['email']} bsSize="md" />
+                    </Col>}
+                  </Row>
+                )
+              })}
+
+              <Row>
+                <Col sm={{ size: 2, offset: 10 }} className="text-right">
+                  <Button className="mt-4 mx-auto btn-kimlic" size="lg" color="primary">Invite</Button>
+                </Col>
+              </Row>
+            </Container>
+          </Jumbotron>
+        </Container>
+      </div>
+    )
+  }
+}
+
+// Export
+
+export default InviteMembers
--- src/containers/Dashboard/containers/Settings/index.js
+++ src/containers/Dashboard/containers/Settings/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-import { Container, Row, Col, Jumbotron, Form, FormGroup, CustomInput, Label, Input, Button, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap'
+import { Container, Row, Col } from 'reactstrap'
 
 import './index.scss'
 
 import { role, roleLabel } from 'src/constants/auth'
 
-import ImageUploadModal from './components/ImageUploadModal'
+import CompanyDetails from './components/CompanyDetails'
+import InviteMembers from './components/InviteMembers'
 
 // Component
 
@@ -15,92 +16,15 @@ class Settings extends Component {
     dropdownOpen: false
   }
 
+  // Handlers
+
   toggle = () => {
     this.setState(prevState => ({
       dropdownOpen: !prevState.dropdownOpen
-    }));
+    }))
   }
 
   // Render
-
-  renderCompanySettings = () => (
-    <div className="settings--company">
-      <Jumbotron>
-        <h2>Basic Information</h2>
-
-        <Form>
-          <Container fluid>
-            <FormGroup row>
-              <Col sm={6}>
-                <Input type="text" name="name" id="name" placeholder="Company Name" bsSize="md" />
-              </Col>
-              <Col sm={6}>
-                <Input type="text" name="client" id="client" placeholder="Client ID" bsSize="md" disabled />
-              </Col>
-            </FormGroup>
-
-            <FormGroup row>
-              <Col sm={6}>
-                <Input type="url" name="domain" id="domain" placeholder="Domain Address" bsSize="md" />
-              </Col>
-
-              <Col sm={6}>
-                <Input type="text" name="session" id="session" placeholder="Session Type" bsSize="md" />
-              </Col>
-            </FormGroup>
-
-            <FormGroup row>
-              <Col sm={6}>
-                <Input type="email" name="email" id="email" placeholder="Email Address" bsSize="md" />
-              </Col>
-
-              <Col sm={6}>
-                <ImageUploadModal />
-              </Col>
-            </FormGroup>
-
-            <Row>
-              <Col sm={{ size: 2, offset: 10 }} className="text-right">
-                <Button className="mt-4 mx-auto btn-kimlic" size="lg" color="primary" type="submit">Submit</Button>
-              </Col>
-            </Row>
-          </Container>
-        </Form>
-      </Jumbotron>
-    </div>
-  )
-
-  renderAccessManagement = (members) => (
-    <div className="settings--access">
-      <Jumbotron>
-        <h2>Basic Information</h2>
-
-        <Container fluid>
-          {members.map((el, idx) => {
-            return (
-              <Row key={idx}>
-                <Col sm={6}>
-                  <Label for="email">{el[0]['role']}</Label>
-                  <Input type="email" name="email" id="email" placeholder="Email" value={el[0]['email']} bsSize="md" />
-                </Col>
-
-                {el[1] && <Col sm={6}>
-                  <Label for="email">{el[1]['role']}</Label>
-                  <Input type="email" name="email" id="email" placeholder="Email" value={el[1]['email']} bsSize="md" />
-                </Col>}
-              </Row>
-            )
-          })}
-
-          <Row>
-            <Col sm={{ size: 2, offset: 10 }} className="text-right">
-              <Button className="mt-4 mx-auto btn-kimlic" size="lg" color="primary">Invite</Button>
-            </Col>
-          </Row>
-        </Container>
-      </Jumbotron>
-    </div>
-  )
 
   render() {
     const members = [
@@ -118,8 +42,19 @@ class Settings extends Component {
 
     return (
       <div className="settings">
-        {this.renderCompanySettings()}
-        {this.renderAccessManagement(memberList)}
+        <Container fluid>
+          <Row>
+            <Col xs={12}>
+              <CompanyDetails />
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs={10}>
+              <InviteMembers members={memberList} />
+            </Col>
+          </Row>
+        </Container>
       </div>
     )
   }
