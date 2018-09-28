e8c20d3664d4ac3933c11b41a58b525ad408cacc
Dmytro Nasyrov <dmytro@pharosproduction.com>
2018-09-28 17:09:46 +0300

    hardcode api url

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ const authLink = setContext((_, { headers }) => {
 
 const client = new ApolloClient({
   link: authLink
-    .concat(new createUploadLink({ uri: process.env.API_URI })),
+    .concat(new createUploadLink({ uri: 'https://demo.kimlic.com:4003/api' })),
   cache: new InMemoryCache()
 })
 
