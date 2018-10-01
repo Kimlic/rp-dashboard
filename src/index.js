a3d454434a9d436fe0bb46d9ab86e22dd2661bb7
Dmytro Nasyrov <dmytro@pharosproduction.com>
2018-10-01 12:14:55 +0300

    change url

--- src/index.js
+++ src/index.js
@@ -37,8 +37,8 @@ const authLink = setContext((_, { headers }) => {
 
 const client = new ApolloClient({
   link: authLink
-    .concat(new createUploadLink({ uri: 'http://localhost:4003/api' })),
-    // .concat(new createUploadLink({ uri: 'https://demo.kimlic.com/api' })),
+    // .concat(new createUploadLink({ uri: 'http://localhost:4003/api' })),
+    .concat(new createUploadLink({ uri: 'https://demo.kimlic.com/api' })),
   cache: new InMemoryCache()
 })
 
