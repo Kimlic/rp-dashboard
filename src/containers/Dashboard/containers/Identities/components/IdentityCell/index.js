87c8e24f94dbb16cb6060bbbc695261b76fa4c86
Dmytro Nasyrov <dmytro@pharosproduction.com>
2018-10-12 12:00:18 +0300

    add pending status

--- src/containers/Dashboard/containers/Identities/components/IdentityCell/index.js
+++ src/containers/Dashboard/containers/Identities/components/IdentityCell/index.js
@@ -10,7 +10,27 @@ import { prettifyDate } from 'src/utils/date'
 class IdentityCell extends Component {
 
   render() {
-    const { firstName, lastName, type, insertedAt, verifiedAt, verified } = this.props.document
+    const { firstName, lastName, type, insertedAt, verifiedAt, status } = this.props.document
+    let statusStr
+    let circleClass
+
+    switch (status) {
+      case null:
+        statusStr = "Pending"
+        circleClass = "circle__pending" 
+        break
+
+      case "approved": 
+        statusStr = "Verified"
+        circleClass = "circle__verified" 
+        break
+
+      case "declined":
+      case "resubmission_requested": 
+        statusStr = "Unverified"
+        circleClass = "circle__unverified" 
+        break
+    }
 
     return (
       <div className="identity-cell">
@@ -38,9 +58,9 @@ class IdentityCell extends Component {
 
             <Col xs={2} className="col__verified">
               <svg xmlns="http://www.w3.org/2000/svg">
-                <circle cx="50%" cy="50%" r="5" className={verified ? "circle__verified" : "circle__unverified"} />
+                <circle cx="50%" cy="50%" r="5" className={circleClass} />
               </svg>
-              <b>{verified ? "Verified" : "Unverified"}</b>
+              <b>{statusStr}</b>
             </Col>
           </Row>
         </Container>
